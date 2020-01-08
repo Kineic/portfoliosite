92a7163998b2110283092a85ad4b6e413afc79b5
Gareth <garethcraig97@gmail.com>
2020-01-08 15:42:47 +0000

    add profile photo, fixed work alignment boxes

--- Portfolio Site/js/javascript.js
+++ Portfolio Site/js/javascript.js
@@ -1,53 +1,53 @@
-function showUXUIWork(){
-	showWork("exUI");
-	hideWork("showMotionVideo");
-	hideWork("cadDesign");
-	hideWork("graphicDesign");
-	hideWork("coding");
+function showUXUIWork() {
+    showWork("exUI");
+    hideWork("showMotionVideo");
+    hideWork("cadDesign");
+    hideWork("graphicDesign");
+    hideWork("coding");
 }
 
-function showMotionVideoWork(){
-	showWork("showMotionVideo");
-	hideWork("cadDesign");
-	hideWork("uxUI");
-	hideWork("graphicDesign");
-	hideWork("coding");
+function showMotionVideoWork() {
+    showWork("showMotionVideo");
+    hideWork("cadDesign");
+    hideWork("uxUI");
+    hideWork("graphicDesign");
+    hideWork("coding");
 }
 
-function showGraphicDesignWork(){
-	showWork("graphicDesign");
-	hideWork("showMotionVideo");
-	hideWork("uxUI");
-	hideWork("cadDesign");
-	hideWork("coding");
+function showGraphicDesignWork() {
+    showWork("graphicDesign");
+    hideWork("showMotionVideo");
+    hideWork("uxUI");
+    hideWork("cadDesign");
+    hideWork("coding");
 }
 
-function showCodingWork(){
-	showWork("coding");
-	hideWork("showMotionVideo");
-	hideWork("uxUI");
-	hideWork("graphicDesign");
-	hideWork("cadDesign");
-	}
+function showCodingWork() {
+    showWork("coding");
+    hideWork("showMotionVideo");
+    hideWork("uxUI");
+    hideWork("graphicDesign");
+    hideWork("cadDesign");
+}
 
 function showCADWork() {
-	showWork("cadDesign");
-	hideWork("showMotionVideo");
-	hideWork("uxUI");
-	hideWork("graphicDesign");
-	hideWork("coding");
+    showWork("cadDesign");
+    hideWork("showMotionVideo");
+    hideWork("uxUI");
+    hideWork("graphicDesign");
+    hideWork("coding");
 }
 
 function showWork(className) {
-	var list = document.getElementsByClassName(className);
-	for(i = 0; i < list.length; i++){
-		list[i].style.display = "inline-block";
-	}
+    var list = document.getElementsByClassName(className);
+    for (i = 0; i < list.length; i++) {
+        list[i].style.display = "inline-block";
+    }
 }
 
 function hideWork(className) {
-	var list = document.getElementsByClassName(className);
-	for(i = 0; i < list.length; i++){
-		list[i].style.display = "none";
-	}
+    var list = document.getElementsByClassName(className);
+    for (i = 0; i < list.length; i++) {
+        list[i].style.display = "none";
+    }
 }
